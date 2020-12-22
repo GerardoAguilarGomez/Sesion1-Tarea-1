1af1e252059c70cffa67bc2e3e927bab9ac81567
Gerardo Aguilar Gomez <gerardo.aguilar@grupocastilla.es>
2020-12-22 13:18:43 +0100

    Agregar archivos de proyecto.

---
+++ Sesion1 Tarea 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sesion1_Tarea_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
